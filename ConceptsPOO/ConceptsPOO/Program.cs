4f2cb728b85548620807a3bef3356f56ff2f107b
JOSE IGNACIO CARDONA CAICEDO <jose.cardona@umayor.cl>
2022-02-26 01:00:44 -0300

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+try
+{
+    Console.WriteLine(new Date(2022, 2, 11));
+    Console.WriteLine(new Date(1976, 6, 39));
+    Console.WriteLine(new Date(1984, 45, 2));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message); 
+}
